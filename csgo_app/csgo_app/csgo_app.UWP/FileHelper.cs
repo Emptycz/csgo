271b0cd5a420a4d2936c180ecf4a04dcdf12729d
Jan Kočvara <kimichisxd285@gmail.com>
2017-06-04 15:13:04 +0200

    Přidání FileHelper do UWP

---
+++ csgo_app/csgo_app/csgo_app.UWP/FileHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using csgo_app.UWP;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace csgo_app.UWP
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}
